82873d07ce5b389d6f18cd9c7af4a81989c86d0e
zach <etta1055@gmail.com>
2026-03-13 14:30:06 -0500

    sanity updates

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,7 +3,10 @@ import { defineCliConfig } from 'sanity/cli';
 
 export default defineCliConfig({
   api: {
-    projectId: 'tym561ok', 
+    projectId: 'tym561ok',
     dataset: 'production'
+  },
+  deployment: {
+    appId: 'xksibnjk6y9w40z2ys7en979',
   }
 });
